668bd59a78bd0001a1a271e8ed49c5d6a1ef5f53
Joe Egboka <joeegboka@gmail.com>
2023-09-18 11:47:44 +0100

    add main.c file

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 when success or 1 on error
+ */
+int main(int argc, char **argv)
+{
+	info_t info[] = { _INIT_ };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+	if (argc == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_puts(av[0]);
+				_puts(": 0: Can't open ");
+				_puts(av[1]);
+				_putchar('\n');
+				_putchar(BUFFER_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	create_env_list(info);
+	rd_history(info);
+	hsh(info, argv);
+
+	return (EXIT_SUCCESS);
+}
+
